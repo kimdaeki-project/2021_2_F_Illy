e63044702b8d39e90963c9a2f6cc0cc2f5083328
neoneoneoguli <myj4100@gmail.com>
2021-11-12 16:36:14 +0900

    Initial commit

---
+++ src/main/java/com/c/illy/ProjectIllyApplication.java
@@ -0,0 +1,13 @@
+package com.c.illy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectIllyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectIllyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/c/illy/ProjectIllyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c.illy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectIllyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
